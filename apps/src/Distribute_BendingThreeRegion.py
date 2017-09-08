b4928846d19638ab89799903c6be113e5b659f77
Fergus Cooper <cooper@maths.ox.ac.uk>
2017-09-08 10:27:26 +0100

    Add new varaibles

--- apps/src/Distribute_BendingThreeRegion.py
+++ apps/src/Distribute_BendingThreeRegion.py
@@ -39,30 +39,31 @@ if not(os.path.isfile(executable)):
     quit('Py: Could not find executable: ' + executable)
 
 # List of command line arguments for the executable, and corresponding list of parameter names
-command_line_args = [' --ID ', ' --CRL ', ' --CSC ', ' --TRL ', ' --TSC ', ' --AD ', ' --DI ', ' --SM ', ' --NS ',
-                     ' --RM ', ' --TS ', ' --AL ']
+command_line_args = [' --ID ', ' --CRL ', ' --CSC ', ' --TRL ', ' --TSC ', ' --KFS ', ' --ALM ', ' --DI ', ' --SM ',
+                     ' --NS ', ' --RM ', ' --TS ', ' --AL ']
 params_list = ['simulation_id', 'cor_rest_length', 'cor_spring_const', 'tra_rest_length', 'tra_spring_const',
-               'adhesion_modifier', 'interaction_dist', 'stiffness_mult', 'normal_std', 'remesh_frequency',
-               'num_time_steps', 'apical_lamina']
+               'kinematic_strength', 'apical_lam_mult', 'interaction_dist', 'stiffness_mult', 'normal_std',
+               'remesh_frequency', 'num_time_steps', 'apical_lamina']
 
 # Time string when script is run, used for creating a unique archive name
 today = time.strftime('%Y-%m-%dT%H%M')
 
 # Param ranges (in lists, for itertools product)
 crl = [0.25]
-csc = np.linspace(1.0 * 1e8, 1.1 * 1e8, num=1)
+csc = np.linspace(1.6 * 1e8, 2.0 * 1e8, num=1)
 trl = [1.0]
-tsc = np.linspace(0.8 * 1e7, 1.0 * 1e7, num=1)
-ad = [1.0]
+tsc = np.linspace(0.5 * 1e7, 1.0 * 1e7, num=1)
+kfs = np.linspace(1.0 * 1e4, 7.0 * 1e4, num=1)
+alm = np.linspace(0.2, 0.2, num=1)
 di = [0.02]
-sm = np.linspace(0.1, 0.4, num=4)
-ns = [0.03]
+sm = np.linspace(0.4, 0.2, num=2)
+ns = [0.00]
 rf = [50]
-ts = [70000]
+ts = [1000]
 al = [True]
 
 # An enumerated iterable containing every combination of the parameter ranges defined above
-combined_iterable = list(itertools.product(crl, csc, trl, tsc, ad, di, sm, ns, rf, ts, al))
+combined_iterable = list(itertools.product(crl, csc, trl, tsc, kfs, alm, di, sm, ns, rf, ts, al))
 
 
 def main():
